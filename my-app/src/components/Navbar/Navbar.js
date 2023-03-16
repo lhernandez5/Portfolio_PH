f32c262e99b61868eb3a59ae290d91fc928f0ef1
Luz Hernandez <hernandezluz18@gmail.com>
2023-03-16 13:30:10 -0400

    added css framework

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,3 +1,11 @@
 export default function Navbar(){
-    return(<></>);
+    return(
+    <div class="tabs is-right">
+        <ul>
+            <li class="is-active"><a>About</a></li>
+            <li><a>Projects</a></li>
+            <li><a>Resume</a></li>
+        </ul>
+    </div>
+  );
 }
